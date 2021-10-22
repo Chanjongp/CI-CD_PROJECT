567344eca5dd258e5afc4c8c3c7a92246b909458
Chanjongp <pcj0198@daum.net>
2021-10-23 00:38:27 +0900

    Fix : Add docker command

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it -d -p 8000:7000 --name practice pcj0198/practice
+docker run -it -d -p 7000:8000 --name practice pcj0198/practice
 
 DOCKER_WEB_CONTAINER = $(docker container ls -f "name=practice" -q)
