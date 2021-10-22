f16a530a788f949881ffb07085556b2aacfe1880
Chanjongp <pcj0198@daum.net>
2021-10-22 18:12:56 +0900

    Fix

--- cicd/settings.py
+++ cicd/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fgg+6$s122chq7en)66^tc*)xf(_ss8%9t4yg26h=l4r47li0@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
